eaaa25563d530ddabb9f32694b0b81e7ad233acd
AleksNau <workciti@yandex.ru>
2023-08-03 11:54:37 +0300

    сделать реквесты

--- app.js
+++ app.js
@@ -4,32 +4,60 @@ mongoose.connect('mongodb://localhost:27017/mestodb',{
   console.log('mangoo включено')
 })
 
-const express = require('express')
-const { default: mongoose } = require('mongoose')
-const app = express()
-const port = 3000
-const path = require('path')
-const cards = require('./cards.json');
-const bodyParser = require('body-parser')
+const express = require('express');
+const { default: mongoose } = require('mongoose');
 
-const user = require('./models/user')
-const card = require('./models/cards')
+const app = express();
+const port = 3000;
+const path = require('path');
+
+const cards = require('./cards.json');
+const bodyParser = require('body-parser');
+
+const userModel = require('./models/user');
+const cardModel = require('./models/cards');
+
 app.use(bodyParser.json());
 //получить всех пользователей
 app.get('/users', (req, res) => {
-  res.status(201).send('профиль пользователя')
+  return userModel.find()
+      .then((users)=> {
+        res.status(200).send(users);
+      })
+      .catch((err) => res.status(500).send('server error'))
+  
 })
 //получить пользователя по id
-app.get('/users/:userId', (req, res) => {
-  res.status(200).send('профиль пользователя')
+app.get('/users/:id', (req, res) => {
+  const {id} = req.params;
+  return userModel.findById(id)
+      .then((user)=> {
+        if(!user) {
+          return res.status(484).send('server error');
+        }
+        res.status(200).send(user);
+      })
+      .catch((err) => res.status(500).send('server error'))
 })
 //отправка карточек
 app.get('/cards', (req, res) => {
-  res.status(200).send(cards)
+  return res.status(200).send(cards)
 })
 //создание пользователя
 app.post('/users', (req, res) => {
-  res.send('регистрация')
+  return userModel.create({...req.body})
+  .then((user)=> {
+    res.status(201).send(user);
+  })
+  .catch((err) => {
+    if (err.name === "ValidationError") {
+      return res.status(400).send({
+          message: `${Object.values(err.errors).map((err) => err.message).join(", ")}`
+      });}
+    console.error();
+    return res.status(500).send('server error')//должна быть 400 ошибка
+  })
+  res.send('регистрация');
 })
 
 
