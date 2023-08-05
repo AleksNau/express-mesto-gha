8787d76e18dcdd7bb60d8eca0db4dbcf2d9b88db
AleksNau <workciti@yandex.ru>
2023-08-05 15:33:54 +0300

    сделать контролеры пользователя

---
+++ controllers/users.js
@@ -0,0 +1,58 @@
+const userModel = require('../models/user');
+
+const createProfile = (req, res) => {
+  return userModel.create({...req.body})
+    .then((user) => {
+      res.status(201).send(user);
+    })
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        return res.status(400).send({
+          message: `${Object.values(err.errors).map((err) => err.message).join(", ")}`
+        });
+      }
+      console.error();
+      return res.status(500).send('server error')//должна быть 400 ошибка
+    })
+}
+
+const getProfileById = (req, res) => {
+  const {id} = req.params;
+  return userModel.findById(id)
+    .then((user) => {
+      if (!user) {
+        return res.status(484).send('server error');
+      }
+      res.status(200).send(user);
+    })
+    .catch((err) => res.status(500).send('server error'))
+  //404,500
+}
+
+const getUsersList = (req, res) => {
+  return userModel.find()
+    .then((users) => {
+      res.status(200).send(users);
+    })
+    .catch((err) => res.status(500).send('server error'))
+  //400,500
+}
+
+const updateProfile = (req, res) => {
+  res.send('запрос обновляет информацию о пользователе.')
+  //400,404,500
+}
+
+const changeAvatar = (req, res) => {
+  res.send('запрос обновляет аватар пользователя.')
+  //400,404,500
+}
+
+
+module.exports = {
+  createProfile,
+  getProfileById,
+  getUsersList,
+  updateProfile,
+  changeAvatar
+}
--- routes/users.js
+++ routes/users.js
@@ -1,52 +1,15 @@
 const router = require('express').Router();
-const userModel = require('../models/user');
+const { createProfile, getProfileById, getUsersList, updateProfile, changeAvatar} = require("../controllers/users");
 //создание пользователя
-router.post('/users', (req, res) => {
-    return userModel.create({...req.body})
-    .then((user)=> {
-      res.status(201).send(user);
-    })
-    .catch((err) => {
-      if (err.name === "ValidationError") {
-        return res.status(400).send({
-            message: `${Object.values(err.errors).map((err) => err.message).join(", ")}`
-        });}
-      console.error();
-      return res.status(500).send('server error')//должна быть 400 ошибка
-    })
-  })
+router.post('/users', createProfile)
 //получить пользователя по id
-  router.get('/users/:id', (req, res) => {
-    const {id} = req.params;
-    return userModel.findById(id)
-        .then((user)=> {
-          if(!user) {
-            return res.status(484).send('server error');
-          }
-          res.status(200).send(user);
-        })
-        .catch((err) => res.status(500).send('server error'))
-        //404,500
-  })
+  router.get('/users/:id', getProfileById)
 
   //получить всех пользователей
-  router.get('/users', (req, res) => {
-    return userModel.find()
-        .then((users)=> {
-          res.status(200).send(users);
-        })
-        .catch((err) => res.status(500).send('server error'))
-    //400,500
-  })
+  router.get('/users', getUsersList)
 
-  router.patch('/users/me', (req, res) => {
-    res.send('запрос обновляет информацию о пользователе.')
-    //400,404,500
-  })
+  router.patch('/users/me', updateProfile)
 
-  router.patch('/users/me/avatar', (req, res) => {
-    res.send('запрос обновляет аватар пользователя.')
-    //400,404,500
-  })
+  router.patch('/users/me/avatar', changeAvatar)
 
   module.exports = router;
