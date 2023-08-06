0c90bffe27331bf97b9c07018e971ee7bd25db78
AleksNau <workciti@yandex.ru>
2023-08-06 10:48:36 +0300

    тест

--- controllers/users.js
+++ controllers/users.js
@@ -28,7 +28,13 @@ const getProfileById = (req, res) => {
       }
       res.status(200).send(user);
     })
-    .catch((err) => res.status(500).send(serverError))
+    .catch((err) => {
+      if (err.name === "CastError") {
+        return res.status(400).send({
+          message: `${Object.values(err.errors).map((err) => err.message).join(", ")}`
+        });
+      }
+      res.status(500).send(serverError)})
   //404,500
 }
 
