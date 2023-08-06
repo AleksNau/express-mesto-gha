d88426a88fb28607973e20315d55e2c8028d8021
AleksNau <workciti@yandex.ru>
2023-08-06 10:31:51 +0300

    тест

--- controllers/cards.js
+++ controllers/cards.js
@@ -32,7 +32,7 @@ const deleteCard = (req, res) => {
   return cardModel.findById(cardId)
     .then((card) => {
       if (!card) {
-        return res.status(400).send(cardNotFound);
+        return res.status(404).send(cardNotFound);
       }
       return cardModel.deleteOne({"_id": cardId}).then(() => res.send({message: 'Карточка успешно удалена'}));
     })
