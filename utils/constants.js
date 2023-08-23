009bb3813fd5cea9e12df9700a28c59494dc7231
AleksNau <workciti@yandex.ru>
2023-08-23 21:47:31 +0300

    delete validationId

--- utils/constants.js
+++ utils/constants.js
@@ -2,16 +2,8 @@ const {
   BadRequestError,
 } = require('../errors/errors');
 
-const validationID = (id) => {
-  if (/^[0-9a-fA-F]{24}$/.test(id)) {
-    return id;
-  }
-  throw new BadRequestError('Передан некорретный id.');
-};
-
 const validationURL = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
 
 module.exports = {
-  validationID,
   validationURL,
 };
