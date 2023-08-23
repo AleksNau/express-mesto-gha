609a2116f2bec7d52a7c4aef97a3cc747cd6492f
AleksNau <workciti@yandex.ru>
2023-08-23 17:34:58 +0300

    regex

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -48,7 +48,7 @@ module.exports.validationUpdateUser = celebrate({
 
 module.exports.validationUpdateAvatar = celebrate({
   body: Joi.object().keys({
-    avatar: Joi.string().required().custom(validationUrl),
+    avatar: Joi.string().regex(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/),
   }),
 });
 // почемуто не работает
@@ -61,7 +61,7 @@ module.exports.validationUserId = celebrate({
 module.exports.validationCreateCard = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30).required(),
-    link: Joi.string().required().custom(validationUrl),
+    link: Joi.string().required().regex(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/),
   }),
 });
 
